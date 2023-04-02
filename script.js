b1c63b7f7794a29bf8480aa723b9adcff9c303f6
noceive <nocivescience@gmail.com>
2023-04-02 04:06:13 -0400

    avanzando en la animacion

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const containerEl=document.getElementById('container');
 const cardContainerEl=document.getElementById('card-container');
 document.addEventListener('click',()=>{
     containerEl.classList.toggle('show');
+    cardContainerEl.classList.toggle('hide');
 });
